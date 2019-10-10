f0bf13c96483f5645e4d36156725c5e619e4a8a1
Corentin Grall <corentin.grall@gmail.com>
2019-10-10 12:51:42 +0200

    credit card input mask

---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+  $('#cardNumber').on('input', function(e) {
+    let value = $('#cardNumber').val();
+  
+  });
+
+  $(":input").inputmask({ "placeholder": "" });
+});
