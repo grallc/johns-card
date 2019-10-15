b4e12c597ff9be00bcb44f2625ae249fdf5d3240
Corentin Grall <corentin.grall@gmail.com>
2019-10-15 23:39:37 +0200

    added another flowchart

---
+++ assets/js/flowchart.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("#large-button").on("click", function() {
+    $("#flowchart-image").attr("src", "assets/images/flowchart-large.png");
+    $(this).addClass("active");
+    $("#compact-button").removeClass("active");
+  });
+
+  $("#compact-button").on("click", function() {
+    $("#flowchart-image").attr("src", "assets/images/flowchart-compact.png");
+    $(this).addClass("active");
+    $("#large-button").removeClass("active");  
+  });
+});
