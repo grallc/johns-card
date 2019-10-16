c603f4320364400f41474de616dc4900f46e6ff4
Corentin Grall <corentin.grall@gmail.com>
2019-10-16 15:59:43 +0200

    jquery bug

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,21 +2,21 @@ $(document).ready(function () {
   $(':input').inputmask({ placeholder: '' })
   $('#cardNumber').on('input', function (e) {
     const value = $('#cardNumber').val()
-
     if (value.charAt(0) == '4') {
-      $('#credit-card-type').show()
+      $('#credit-card-type').removeClass("hidden");
       $('#credit-card-type').attr('src', 'assets/images/visa.png')
     } else if (value.charAt(0) == '5') {
-      $('#credit-card-type').show()
+      $('#credit-card-type').removeClass("hidden");
       $('#credit-card-type').attr('src', 'assets/images/master-card.png')
     } else if (value.charAt(0) == '6') {
-      $('#credit-card-type').show()
+      $('#credit-card-type').removeClass("hidden");
       $('#credit-card-type').attr('src', 'assets/images/discover.png')
     } else if (value.length >= 2 && value.charAt(0) == '3' && value.charAt(1) == '7') {
-      $('#credit-card-type').show()
+      $('#credit-card-type').removeClass("hidden");
       $('#credit-card-type').attr('src', 'assets/images/amex.png')
     } else {
-      $('#credit-card-type').hide()
+      $('#credit-card-type').addClass("hidden");
     }
+    console.log(value.charAt(0))
   })
 })
